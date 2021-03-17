61b0d15307302839373dae54ad791910124d71b2
charles-ekube <charleswakili797@gmail.com>
2021-03-17 09:54:29 +0000

    worked on the responsiveness of the webpages

--- src/Widgets/Navigation/Navbar.js
+++ src/Widgets/Navigation/Navbar.js
@@ -1,8 +1,55 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { FiMenu } from "react-icons/fi";
 import './Navbar.css';
 
 const Navbar = ({navColor}) => {
+
+    const [showMenu, setShowMenu] = React.useState(false);
+    let menu
+    // let menuMask
+
+    if(showMenu) {
+        menu = <aside className='mobile-nav-container'>
+            <header>
+                <nav>
+                    <div>
+                        <Link to='/' style={{color:navColor}}>Logo</Link>
+                    </div>
+                    <ul>
+                        <li>
+                            <Link to='/' style={{color:navColor}}>
+                                Home
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to='/Projects' style={{color:navColor}}>
+                                Projects
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to='/Explore' style={{color:navColor}}>
+                                Explore
+                            </Link>
+                        </li>
+                        <li>
+                            <Link style={{color:navColor}}>
+                                Blog
+                            </Link>
+                        </li>
+                    </ul>
+                </nav>
+            </header>
+        </aside>
+    }
+
+
+
+    // menuMask = <aside
+    // className="mobile-menu-mask"
+    // onClick={() => setShowMenu(false)}></aside>
+
+
     return (
         <>
             <header>
@@ -38,8 +85,12 @@ const Navbar = ({navColor}) => {
                             <input />
                         </form>
                     </div>
+                    <div className='mobile-nav-btn'>
+                        <FiMenu onClick={() => setShowMenu(!showMenu)}/>
+                    </div>
                 </nav>
             </header>
+            {menu}
         </>
     )
 }
